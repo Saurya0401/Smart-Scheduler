ebc69545bdcdc46058d2beb7139117f0794e5743
Saurya0401 <sp04012000@gmail.com>
2021-10-27 14:49:33 +0800

    Added test case docstrings and made minor improvements.

--- test/test_database.py
+++ test/test_database.py
@@ -8,6 +8,7 @@ from smartscheduler.exceptions import CommonDatabaseError
 
 
 class DatabaseTest(unittest.TestCase):
+    """TEST A.1"""
 
     db_path = "./test/test_server/Test.db"
 
@@ -15,7 +16,8 @@ class DatabaseTest(unittest.TestCase):
     def setUpClass(cls):
         SmartSchedulerDB(cls.db_path)
 
-    def test_create_tables(self):
+    def test_a11_create_tables(self):
+        """TEST_CASE_ID A.1.1"""
         db = SmartSchedulerDB(self.db_path)
         conn = sqlite3.connect(self.db_path)
         curs = conn.cursor()
@@ -32,7 +34,8 @@ class DatabaseTest(unittest.TestCase):
         self.assertEqual(retrieved_accounts_cols, account_cols)
         self.assertEqual(retrieved_sub_info_cols, sub_info_cols)
 
-    def test_add_one_data(self):
+    def test_a12_add_one_data(self):
+        """TEST_CASE_ID A.1.2"""
         db = SmartSchedulerDB(self.db_path)
         test_data = [str(randint(10 ** 9, 10 ** 10 - 1)), "test_pass_hash", "test_sch", "test_subs"]
         db.new_account(*test_data)
@@ -43,9 +46,10 @@ class DatabaseTest(unittest.TestCase):
         self.assertRaises(CommonDatabaseError, db.__db_cmd__, duplicate_data_cmd)
         return test_data
 
-    def test_upd_one_data(self):
+    def test_a13_upd_one_data(self):
+        """TEST_CASE_ID A.1.3"""
         db = SmartSchedulerDB(self.db_path)
-        test_data_id = self.test_add_one_data()[0]
+        test_data_id = self.test_a12_add_one_data()[0]
         updated = {
             db.COL_PSWRD_HASH: "updated_pass_hash",
             db.COL_SCHEDULE: "updated_sch",
@@ -60,15 +64,17 @@ class DatabaseTest(unittest.TestCase):
         update_data_cmd = f"UPDATE {db.TAB_ACCOUNTS} SET {invalid_column}='value' WHERE {db.COL_STU_ID}={test_data_id}"
         self.assertRaises(CommonDatabaseError, db.__db_cmd__, update_data_cmd)
 
-    def test_del_one_data(self):
+    def test_a14_del_one_data(self):
+        """TEST_CASE_ID A.1.4"""
         db = SmartSchedulerDB(self.db_path)
-        test_data = self.test_add_one_data()
+        test_data = self.test_a12_add_one_data()
         db.delete_account(test_data[0])
         retrieved_data = [db.query_account_info(test_data[0], query) for query in
                           (db.COL_STU_ID, db.COL_PSWRD_HASH, db.COL_SCHEDULE, db.COL_SUBJECTS)]
         self.assertEqual(all([i == () for i in retrieved_data]), True)
 
-    def test_add_batch_data(self):
+    def test_a15_add_batch_data(self):
+        """TEST_CASE_ID A.1.5"""
         db = SmartSchedulerDB(self.db_path)
         test_list = [(f"TestKey{i}", f"Test Name {i}") for i in range(200)]
         db.make_subs_list(test_list)
--- test/test_utils.py
+++ test/test_utils.py
@@ -7,28 +7,34 @@ from smartscheduler.exceptions import FatalError
 
 
 class UtilsTest(unittest.TestCase):
+    """TEST B.1"""
 
     def setUp(self):
         self.config_path = "./test/test_config.ini"
         self.test_time_str = "1234"
 
-    def test_curr_day_and_time(self):
+    def test_b11_curr_day_and_time(self):
+        """TEST_CASE_ID B.1.1"""
         self.assertEqual(Utils.curr_time(), dt.datetime.now())
         self.assertEqual(Utils.curr_day(), dt.datetime.weekday(dt.datetime.now()))
 
-    def test_time_obj(self):
+    def test_b12_time_obj(self):
+        """TEST_CASE_ID B.1.2"""
         test_time_obj = dt.datetime.strptime(self.test_time_str, "%H%M").time()
         self.assertEqual(Utils.time_obj(self.test_time_str), test_time_obj)
 
-    def test_time_str(self):
+    def test_b13_time_str_conversion(self):
+        """TEST_CASE_ID B.1.3"""
         test_time_str = "12:34"
         self.assertEqual(Utils.time_str("1234"), test_time_str)
 
-    def test_time_sub(self):
+    def test_b14_time_subtraction(self):
+        """TEST_CASE_ID B.1.4"""
         sub_time_obj = dt.datetime.strptime("1204", "%H%M").time()
         self.assertEqual(Utils.time_obj(self.test_time_str, 30), sub_time_obj)
 
-    def test_paths(self):
+    def test_b15_get_paths(self):
+        """TEST_CASE_ID B.1.5"""
         test_paths_tuple = ("test/test_server/Test.db", "test/test_server/test_subjects.csv")
         self.assertEqual(Utils.paths(self.config_path), test_paths_tuple)
         self.assertRaises(FatalError, Utils.paths, "wrong/config_file/path")
@@ -38,7 +44,8 @@ class UtilsTest(unittest.TestCase):
         self.assertRaises(FatalError, Utils.paths, empty_config_file)
         remove(empty_config_file)
 
-    def test_colours(self):
+    def test_b16_get_colours(self):
+        """TEST_CASE_ID B.1.6"""
         test_colours_list = ["#111111", "#222222", "#333333", "#444444", "#555555"]
         self.assertEqual(Utils.colours(self.config_path), test_colours_list)
         self.assertRaises(FatalError, Utils.colours, "wrong/config_file/path")
@@ -48,7 +55,8 @@ class UtilsTest(unittest.TestCase):
         self.assertRaises(FatalError, Utils.colours, empty_config_file)
         remove(empty_config_file)
 
-    def test_fonts(self):
+    def test_b17_get_fonts(self):
+        """TEST_CASE_ID B.1.7"""
         test_fonts_list = ["Arial", ("Arial", 10), ("Arial", 20), ("Arial", 40)]
         self.assertEqual(Utils.fonts(self.config_path), test_fonts_list)
         self.assertRaises(FatalError, Utils.fonts, "wrong/config_file/path")
